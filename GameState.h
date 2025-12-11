d7a63a090a7ba490ad19e9c92d902e972b0f4b00
Jakub Kałuski <majkuba2006@gmail.com>
2025-12-11 17:52:59 +0100

    Modyfikacja gamestate

--- GameState.h
+++ GameState.h
@@ -30,10 +30,8 @@ public:
 		blocks.clear();
 		for (const auto& brick : bloki)
 		{
-			// Zapisujemy tylko te bloki, które nie są zniszczone
 			if (!brick.checkDestroy())
 			{
-				// Brick dziedziczy po RectangleShape, więc ma getPosition()
 				blocks.push_back({ brick.getPosition().x, brick.getPosition().y, brick.getHP()});
 			}
 		}
@@ -49,10 +47,8 @@ public:
 		file << "BALL " <<ballPosition.x << " " << ballPosition.y << " "
             << ballVelocity.x << " " << ballVelocity.y << "\n";
 
-        // Format: BLOCKS_COUNT n
         file << "BLOCKS_COUNT " << blocks.size() << "\n";
 
-        // Lista bloków
         for (const auto& b : blocks)
         {
             file << b.x << " " << b.y << " " << b.hp << "\n";
@@ -63,7 +59,6 @@ public:
         return true;
     }
 
-    // ZADANIE 3: Odczyt z pliku
     bool loadFromFile(const std::string& filename)
     {
         std::ifstream file(filename);
@@ -75,13 +70,10 @@ public:
 
         std::string label;
 
-        // Wczytaj Paletkę
         if (!(file >> label >> paddlePosition.x >> paddlePosition.y)) return false;
 
-        // Wczytaj Piłkę
         if (!(file >> label >> ballPosition.x >> ballPosition.y >> ballVelocity.x >> ballVelocity.y)) return false;
 
-        // Wczytaj Bloki
         int count;
         if (!(file >> label >> count)) return false;
 
@@ -99,27 +91,20 @@ public:
         return true;
     }
 
-    // ZADANIE 3: Aplikowanie stanu do gry
     void apply(Paddle& pal, Ball& pilka, std::vector<Brick>& blokiRef)
     {
-        // 1. Przywróć paletkę
         pal.setPosition(paddlePosition.x, paddlePosition.y);
 
-        // 2. Przywróć piłkę
         pilka.setPosition(ballPosition.x, ballPosition.y, ballVelocity.x, ballVelocity.y);
 
-        // 3. Odtwórz bloki
         blokiRef.clear();
 
-        // Obliczamy rozmiar bloku tak jak w Game.h (lub hardcodujemy jeśli stały)
-        // W Game.h: width=640, col=6. (640 - (5*2))/6 ≈ 105.f
         float rozmiar_bloku_x = (640.f - (6 - 1) * 2.f) / 6;
         float rozmiar_bloku_y = 25.f;
         sf::Vector2f size{ rozmiar_bloku_x, rozmiar_bloku_y };
 
         for (const auto& data : blocks)
         {
-            // Tworzymy nowy blok w miejscu wczytanym z pliku
             blokiRef.emplace_back(sf::Vector2f{ data.x, data.y }, size, data.hp);
         };
     }
